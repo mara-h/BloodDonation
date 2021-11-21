2d5f02adfd0abb350c99ae2d1cca1c5dc96ed2d7
marah <mara.husar@gmail.com>
2021-11-21 17:30:18 +0200

    project initialization

---
+++ src/test/java/com/blooddonation/BloodDonationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blooddonation;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
