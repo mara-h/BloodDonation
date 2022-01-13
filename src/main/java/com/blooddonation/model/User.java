174671de6f96fa8b9707b64dac089bc5776e6c35
marah <mara.husar@gmail.com>
2022-01-13 15:09:05 +0200

    added medic

--- src/main/java/com/blooddonation/model/User.java
+++ src/main/java/com/blooddonation/model/User.java
@@ -1,6 +1,4 @@
 package com.blooddonation.model;
-
-
 import javax.persistence.*;
 import java.util.List;
 import java.util.UUID;
@@ -14,6 +12,7 @@ public class User {
     private String password;
     private List<UUID> questionnairesIds;
     private List<UUID> appointmentIds;
+    private boolean isMedic;
 
     @Enumerated(EnumType.STRING)
     private Enums.Sex sex; // if null -> it's general
@@ -36,6 +35,7 @@ public class User {
         this.bloodGroup = bloodGroup;
         this.age = age;
         this.cnp = cnp;
+        this.isMedic = false;
     }
 
     public UUID getId() {
@@ -124,5 +124,13 @@ public class User {
 
     public void setAppointmentIds(List<UUID> appointmentIds) {
         this.appointmentIds = appointmentIds;
+    }
+
+    public boolean isMedic() {
+        return isMedic;
+    }
+
+    public void setMedic(boolean medic) {
+        isMedic = medic;
     }
 }
