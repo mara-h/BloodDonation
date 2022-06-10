d5ee97a495340fefda6fa503ab77c14f08cece4b
marah <mara.husar@gmail.com>
2022-06-10 14:28:25 +0300

    encryption try 1

--- src/main/java/com/blooddonation/service/UserService.java
+++ src/main/java/com/blooddonation/service/UserService.java
@@ -9,7 +9,6 @@ import com.blooddonation.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
